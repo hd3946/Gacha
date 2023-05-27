6516091d04f5e93f092fe515be7bd3f1284ea0f9
Seuhye <seuhye@github.com>
2023-05-28 08:40:21 +0900

    ESlint가 import/order 문제 있다고 찡찡거려서 순서 바꿨다.

--- apps/wap/src/app/page.tsx
+++ apps/wap/src/app/page.tsx
@@ -1,6 +1,6 @@
+import Image from 'next/image'
 import { sayHello } from '../lib'
 import { Button } from '../ui'
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
