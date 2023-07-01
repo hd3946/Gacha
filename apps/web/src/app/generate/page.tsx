2a723e6d0e0274f7cd5487fc39a20cc5900d99a6
SIRI-WORKSTATIO\siri <kjs432117@gmail.com>
2023-07-01 22:36:54 +0900

    1. generate 페이지 추가  - settings 탭  - export 탭 추가  - 탭기능 추가

--- apps/web/src/app/generate/page.tsx
+++ apps/web/src/app/generate/page.tsx
@@ -1,12 +1,28 @@
 'use client'
-import useStoreGeneratorTabs from '@/storeGenerateTabs'
+import Export from '@/components/Export'
+import Header from '@/components/HeaderV2'
+import Organize from '@/components/Organize'
+import Preview from '@/components/Preview'
+import Settings from '@/components/Settings'
+import { useTabValueStore } from '@/store'
+import { TabV2 } from '@/type'
 
 export default function Home() {
-  const { CurrentTabPage } = useStoreGeneratorTabs()
+  const store = useTabValueStore()
+  const TabValue = store.value
   return (
     <>
+      <header>
+        <Header />
+      </header>
+
       <main>
-        <CurrentTabPage />
+        <div className="bg-gray-100 text-gray-700 dark:bg-brand-900 dark:text-[#f3f4f8] border-t border-gray-300 dark:border-[#1d2034] transition-colors duration-200 ease-in-out">
+          {TabValue === TabV2.Organize && <Organize />}
+          {TabValue === TabV2.Preview && <Preview />}
+          {TabValue === TabV2.Export && <Export />}
+          {TabValue === TabV2.Settings && <Settings />}
+        </div>
       </main>
     </>
   )
