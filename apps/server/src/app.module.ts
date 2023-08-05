6bd46c29c0085b21bf1658f78ebe52b64b48f77f
momoim <hd3946@naver.com>
2023-08-05 18:07:02 +0900

    server prettier 버젼 업

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,27 +1,30 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose'
-import { ServeStaticModule } from '@nestjs/serve-static'
-import { join } from 'path'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { CountryModule } from './country/country.module'
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CountryModule } from './country/country.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://tester:000099@groundwater.iptime.org:27017/?authMechanism=DEFAULT', {
-      dbName: 'gachaFools'
-    }),
+    MongooseModule.forRoot(
+      'mongodb://tester:000099@groundwater.iptime.org:27017/?authMechanism=DEFAULT',
+      {
+        dbName: 'gachaFools',
+      },
+    ),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'static')
+      rootPath: join(__dirname, '..', 'static'),
     }),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    CountryModule
+    CountryModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,10 @@
-import { ConfigService } from '@nestjs/config'
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
-  const configService: ConfigService = app.get(ConfigService)
-  await app.listen(configService.get<string>('PORT') || 4000)
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService: ConfigService = app.get(ConfigService);
+  await app.listen(configService.get<string>('PORT') || 4000);
 }
-bootstrap()
+bootstrap();
