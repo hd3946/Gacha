847d4a81b2a92bbc69ea576a8cbd246b2537a75c
momoim <hd3946@naver.com>
2023-08-27 21:36:40 +0900

    colorMode icon 경고에러 발생으로 mounted 되기전에 default 아이콘 추가 각 헤더 버튼 focus 유지 css 추가

--- apps/web/src/components/header/HeaderGenerate.tsx
+++ apps/web/src/components/header/HeaderGenerate.tsx
@@ -1,5 +1,6 @@
 'use client'
 import useColorMode from '@/hooks/useColorMode'
+import useMounted from '@/hooks/useMouted'
 import { useTabValueStore } from '@/store/store'
 import { GenerateTab } from '@/types/taps'
 import ImageLogo from 'next/image'
@@ -32,23 +33,31 @@ export default function Header() {
     <div className="hiddenScrollbar relative mx-auto flex h-full max-w-[3000px] items-center justify-between overflow-auto bg-red-200 px-2 dark:bg-gray-800 sm:px-6 lg:px-8">
       <div className="flex flex-1 items-center justify-center gap-10 sm:items-stretch sm:justify-start">
         <div className="flex flex-shrink-0 items-center">
-          <Link href="/">
+          <Link href="/" passHref>
             <ImageLogo src={logo} alt="Gacha_Logo" width={220} height={40} priority />
           </Link>
         </div>
 
         <div className="relative z-0 flex flex-1 items-center justify-center">
           <div className="mx-auto my-2 flex space-x-4">
-            <button className="settings_btn group" onClick={() => store.setValue(GenerateTab.Settings)}>
+            <button
+              className={`settings_btn group ${
+                tabNumber === GenerateTab.Settings ? 'border-orange-400 bg-gray-200' : ''
+              }`}
+              onClick={() => setTabNumber(GenerateTab.Settings)}
+              aria-current="page"
+              aria-disabled="false">
               <span className="flex items-center justify-center gap-3 text-orange-400 group-hover:text-orange-300 group-hover:text-orange-300 group-hover:text-orange-400 dark:text-orange-300 dark:text-orange-300">
                 <span className="inline-flex items-center text-orange-400 group-hover:text-orange-300 group-hover:text-orange-300 group-hover:text-orange-400 dark:text-orange-300 dark:text-orange-300">
-                  <IoSettingsOutline className="hi" />
+                  <IoSettingsOutline />
                 </span>
                 <span className="settings_span">Settings</span>
               </span>
             </button>
             <span className="mt-4 text-slate-400"></span>
-            <button className="organize_btn group" onClick={() => store.setValue(GenerateTab.Organize)}>
+            <button
+              className={`organize_btn group ${tabNumber === GenerateTab.Organize && 'border-cyan-400 bg-gray-200'}`}
+              onClick={() => setTabNumber(GenerateTab.Organize)}>
               <span className="flex items-center justify-center gap-3 text-cyan-400 group-hover:text-cyan-400 group-hover:text-cyan-400 group-hover:text-cyan-400 dark:text-cyan-400 dark:text-cyan-400">
                 <span className="inline-flex items-center text-cyan-400 group-hover:text-cyan-400 group-hover:text-cyan-400 group-hover:text-cyan-400 dark:text-cyan-400 dark:text-cyan-400">
                   <IoConstructOutline />
@@ -59,7 +68,9 @@ export default function Header() {
             <span className="mt-4 text-slate-400">
               <svg width="3" height="6" aria-hidden="true" />
             </span>
-            <button className="preview_btn" onClick={() => store.setValue(GenerateTab.Preview)}>
+            <button
+              className={`preview_btn group ${tabNumber === GenerateTab.Preview && 'border-violet-400 bg-gray-200'}`}
+              onClick={() => setTabNumber(GenerateTab.Preview)}>
               <span className="flex items-center justify-center gap-3 text-violet-400 group-hover:text-violet-300 group-hover:text-violet-300 group-hover:text-violet-400 dark:text-violet-300 dark:text-violet-300">
                 <span className="inline-flex items-center text-violet-400 group-hover:text-violet-300 group-hover:text-violet-300 group-hover:text-violet-400 dark:text-violet-300 dark:text-violet-300">
                   <IoFlaskOutline />
@@ -72,7 +83,9 @@ export default function Header() {
             <span className="mt-4 text-slate-400">
               <svg width="3" height="6" aria-hidden="true"></svg>
             </span>
-            <button className="export_btn" onClick={() => store.setValue(GenerateTab.Export)}>
+            <button
+              className={`export_btn group ${tabNumber === GenerateTab.Export && 'border-teal-400 bg-gray-200'}`}
+              onClick={() => setTabNumber(GenerateTab.Export)}>
               <span className="flex items-center justify-center gap-3 text-teal-400 group-hover:text-teal-400 group-hover:text-teal-400 group-hover:text-teal-400 dark:text-teal-400 dark:text-teal-400">
                 <span className="inline-flex items-center text-teal-400 group-hover:text-teal-400 group-hover:text-teal-400 group-hover:text-teal-400 dark:text-teal-400 dark:text-teal-400">
                   <IoSparklesOutline />
@@ -97,13 +110,13 @@ export default function Header() {
         <div className="ml-auto flex items-center justify-end gap-2">
           <button
             className="relative inline-flex h-[36px] w-[36px] flex-shrink-0 cursor-pointer items-center justify-center rounded-md bg-white 
-            text-gray-600 text-gray-900 transition-colors duration-200 ease-in-out hover:bg-gray-200 focus:outline-none 
-            dark:bg-slate-800 dark:text-gray-400 dark:hover:bg-slate-700"
+              text-gray-600 text-gray-900 transition-colors duration-200 ease-in-out hover:bg-gray-200 focus:outline-none 
+              dark:bg-slate-800 dark:text-gray-400 dark:hover:bg-slate-700"
             role="switch"
             type="button"
             aria-checked="true"
             onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
-            {colorMode === 'light' ? <IoSunnyOutline /> : <IoMoonOutline />}
+            {!useMounted() ? <IoSunnyOutline /> : colorMode === 'light' ? <IoSunnyOutline /> : <IoMoonOutline />}
           </button>
           <WalletButton />
         </div>
